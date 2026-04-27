456096e945d0e0684e2340f9c53ac975d864651b
Alfred Marcelo <alfredmarcelo@users.noreply.github.com>
2026-04-27 14:54:21 -0400

    Revert prueba.py to simple subprocess call

---
+++ Centralizador/prueba.py
@@ -0,0 +1,7 @@
+import subprocess
+
+subprocess.run([
+    "C:/Program Files/Google/Chrome/Application/chrome.exe", 
+    "--app=https://www.outlook.com", 
+    "--proxy-server=10.161.146.253:1080"
+]) 
